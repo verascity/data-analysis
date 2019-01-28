211b5aafe43746b6ac7e5e1500ad5d7d1739f430
Vera Sticker <svsticker@gmail.com>
2019-01-28 17:54:30 -0500

    Move code into functions api_setup() and dict_from_api()

--- vaxx-happens/current-tweets.py
+++ vaxx-happens/current-tweets.py
@@ -10,23 +10,39 @@ loads the json file into a Pandas dataframe.
 """
 
 import tweepy
-import json
 import pandas as pd
 import credentials
 
 
-
-auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
-auth.set_access_token(credentials.ACCESS_KEY, credentials.ACCESS_SECRET)
-
-api = tweepy.API(auth, wait_on_rate_limit=True, 
-                 wait_on_rate_limit_notify=True)
-
-tweet_dict = {}
-for tweet in tweepy.Cursor(api.search, q='vaccine -filter:retweets -filter:replies', 
-                           lang='en', tweet_mode='extended', result_type='recent',
-                    geocode='45.7669047,-122.4940866,50mi').items(10):
-    t_id = tweet.id_str
-    tweet_dict[t_id] = tweet.full_text
+def dict_from_api(query, geocode=None, limit=100):
     
-print(tweet_dict)
+    tweet_dict = {}
+    
+    try:
+        tweets = tweepy.Cursor(api_setup().search, q=query, lang='en', 
+    tweet_mode='extended', result_type='recent', geocode=geocode).items(limit)
+    except tweepy.TweepError as err:
+        print(err.api_code)
+    
+    for tweet in tweets:    
+        tweet_dict[tweet.id_str] = tweet.full_text
+        
+    return tweet_dict
+
+def api_setup():
+    
+    auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY, 
+                               credentials.CONSUMER_SECRET)
+    auth.set_access_token(credentials.ACCESS_KEY, 
+                          credentials.ACCESS_SECRET)
+    api = tweepy.API(auth, wait_on_rate_limit=True, 
+                 wait_on_rate_limit_notify=True)
+    
+    return api
+
+
+    
+print(dict_from_api('vaccine -filter:retweets -filter:replies', 
+                    geocode='45.7669047,-122.4940866,50mi', 
+                    limit=2))
+
