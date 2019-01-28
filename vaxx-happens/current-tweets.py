689580a7dce1733b6452e67961172bce5d84fbe8
Vera Sticker <svsticker@gmail.com>
2019-01-28 13:20:11 -0500

    Set up API with credentials

--- vaxx-happens/current-tweets.py
+++ vaxx-happens/current-tweets.py
@@ -12,4 +12,10 @@ loads the json file into a Pandas dataframe.
 import tweepy
 import json
 import pandas as pd
+import credentials
+
+auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
+auth.set_access_token(credentials.ACCESS_KEY, credentials.ACCESS_SECRET)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
