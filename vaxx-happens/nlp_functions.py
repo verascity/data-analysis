016c205808e0c1444394042ceff1dcf0c3da831c
Vera Sticker <svsticker@gmail.com>
2019-02-23 16:55:15 -0500

    Change lemmatizer to stemmer

--- vaxx-happens/nlp_functions.py
+++ vaxx-happens/nlp_functions.py
@@ -8,20 +8,18 @@ anti-vaccination using a Naive Bayes classifier.
 """
 
 from nltk.tokenize import RegexpTokenizer
-from nltk.corpus import stopwords
-from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.stem.snowball import SnowballStemmer
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-def lemmatize(df):
-    wnl = WordNetLemmatizer()
-    df['text_clean'] = (df['text_clean'].apply(lambda x: [wnl.lemmatize(w) for w in x.split(' ')])
-                                        .apply(lambda x: ' '.join(x)))
+def stemmer(df):
+    stemmer = SnowballStemmer('english')
+    df['text_clean'] = df['text_clean'].apply(lambda x: ' '.join([stemmer.stem(w) for w in x.split(' ')]))
     return df
 
 def strip_all(df):
-    df = strip_user_names(df)
-    df = strip_links(df)
+    df = (strip_user_names(df)
+         .pipe(strip_links))
     return df
 
 def strip_links(df):
@@ -40,12 +38,27 @@ def create_clean_text_col(df):
 
 if __name__ == "__main__":
     vaccine_df = strip_all(vaccine_df)
-    vaccine_df = lemmatize(vaccine_df)
+    vaccine_df = stemmer(vaccine_df)
     tknzr = RegexpTokenizer('\w+')
-    vec = TfidfVectorizer(lowercase=True, stop_words="english",
+    vec = TfidfVectorizer(stop_words="english",
                            tokenizer = tknzr.tokenize)
     dtm = vec.fit_transform(vaccine_df['text_clean'])
     vec_df = pd.DataFrame(dtm.toarray(), columns=vec.get_feature_names())
 
     print(vec.get_feature_names())
  #   print(vaccine_df['text_clean'])
+    
+    
+    
+    
+    
+    
+    
+#    X_train, X_test, y_train, y_test = train_test_split(vaccine_df['text_clean'],
+#                                                        vaccine_df['status'],
+#                                                        test_size = 0.33)
+#    tknzr = RegexpTokenizer('\w+')
+#    vec = TfidfVectorizer(lowercase=True, stop_words="english",
+#                           tokenizer = tknzr.tokenize)
+#    bow = vec.fit_transform(vaccine_df['text_clean'])
+#    vec_df = pd.DataFrame(bow.toarray(), columns=vec.get_feature_names())
