5aec07826eaf8aa763df96b80b598c8b0d249f75
Vera Sticker <svsticker@gmail.com>
2019-04-14 20:20:57 -0400

    Create temp housekeeping file

---
+++ vaxx-happens/housekeeping.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 14 20:18:24 2019
+
+@author: svsti
+"""
+
+import pandas as pd
+
+df1 = pd.read_csv('vaccine_df_01312019.csv')
+df2 = pd.read_csv('vaccine_df_02242019.csv')
