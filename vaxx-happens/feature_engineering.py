bb99eb0d23f5fed4c50da01ae63a893dc8765d98
Vera Sticker <svsticker@gmail.com>
2019-04-23 13:02:35 -0400

    Initial upload

---
+++ vaxx-happens/feature_engineering.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 12:51:41 2019
+
+@author: verascity
+
+This module will ultimately contain functions that will create new features
+in a given dataframe, based on certain text characteristics in a column of 
+that dataframe.
+"""
+
