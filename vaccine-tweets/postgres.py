c97ccb1d441074738c528d95edf14c32233b84c4
Vera Sticker <svsticker@gmail.com>
2019-06-19 15:36:03 -0400

    Add defaults to get_connection

--- vaccine-tweets/postgres.py
+++ vaccine-tweets/postgres.py
@@ -11,8 +11,9 @@ collected from current_tweets' Twitter scraper.
 import psycopg2
 
 
-def get_connection(dbname):
+def get_connection(dbname, host='localhost', user='user', password='password'):
 
-    connect_str = "dbname={} host='localhost' user='user' password='password'".format(dbname)
+    connect_str = "dbname={} host={} user={} password={}".format(dbname, host, user, password)
 
     return psycopg2.connect(connect_str)
+
